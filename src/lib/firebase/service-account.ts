a7da9e25b7038a93432ee33e939e8f12246a8102
Igor Imperiali <igor.imperiali@gmail.com>
2025-02-13 14:32:59 -0300

    chore: fix env name

--- src/lib/firebase/service-account.ts
+++ src/lib/firebase/service-account.ts
@@ -1,12 +1,12 @@
-import { VITE_FIREBASE_SERVICE_ACCOUNT } from '$env/static/private';
+import { PRIVATE_FIREBASE_SERVICE_ACCOUNT } from '$env/static/private';
 
 export function getServiceAccount() {
-    if (!VITE_FIREBASE_SERVICE_ACCOUNT) {
+    if (!PRIVATE_FIREBASE_SERVICE_ACCOUNT) {
         throw new Error('Missing Firebase service account configuration');
     }
 
     try {
-        const decoded = Buffer.from(VITE_FIREBASE_SERVICE_ACCOUNT, 'base64').toString();
+        const decoded = Buffer.from(PRIVATE_FIREBASE_SERVICE_ACCOUNT, 'base64').toString();
         return JSON.parse(decoded);
     } catch (error) {
         console.error('Error parsing service account:', error);
