fa54081e6a336f1131683df3de0c0afc0256260d
Igor Imperiali <igor.imperiali@gmail.com>
2025-02-13 18:05:49 -0300

    chore: more polyfill

--- src/lib/polyfills.js
+++ src/lib/polyfills.js
@@ -1,9 +1,19 @@
 import { Buffer } from 'buffer';
 import process from 'process';
 import util from 'util';
+import stream from 'stream';
+import url from 'url';
 
 if (typeof globalThis.Buffer === 'undefined') {
     globalThis.Buffer = Buffer;
+}
+
+if (typeof globalThis.stream === 'undefined') {
+    globalThis.stream = stream;
+}
+
+if (typeof globalThis.url === 'undefined') {
+    globalThis.url = url;
 }
 
 if (typeof globalThis.process === 'undefined') {
