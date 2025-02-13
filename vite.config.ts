77ea855287121e6f41953e449026aa99b321c18a
Igor Imperiali <igor.imperiali@gmail.com>
2025-02-13 15:49:43 -0300

    chore: fix build

--- vite.config.ts
+++ vite.config.ts
@@ -5,28 +5,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit(), tailwindcss()],
 	build: {
-		target: 'esnext',
-		rollupOptions: {
-			external: [
-				'node:crypto',
-				'node:http',
-				'node:https',
-				'node:stream',
-				'node:assert',
-				'node:url',
-				'node:util',
-				'node:net',
-				'node:fs',
-				'node:path',
-				'node:events',
-				'node:buffer',
-				'node:zlib',
-				'node:http2',
-				'node:dns',
-				'node:tls',
-				'node:process'
-			]
-		}
+		target: 'esnext'
 	},
 	ssr: {
 		noExternal: ['firebase', '@firebase/*']
