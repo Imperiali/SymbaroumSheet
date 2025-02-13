c0c04f818b7f46cdbee1709cc513d837684e8fe6
Igor Imperiali <igor.imperiali@gmail.com>
2025-02-13 14:29:18 -0300

    chore: using env sveltkit way

--- src/lib/firebase/admin.config.ts
+++
@@ -1,5 +0,0 @@
-export const adminConfig = {
-    projectId: 'symbaroum-app',
-    clientEmail: import.meta.env.VITE_FIREBASE_CLIENT_EMAIL,
-    privateKey: import.meta.env.VITE_FIREBASE_PRIVATE_KEY
-};
--- src/lib/firebase/service-account.ts
+++ src/lib/firebase/service-account.ts
@@ -1,11 +1,12 @@
+import { VITE_FIREBASE_SERVICE_ACCOUNT } from '$env/static/private';
+
 export function getServiceAccount() {
-    const serviceAccountBase64 = import.meta.env.VITE_FIREBASE_SERVICE_ACCOUNT;
-    if (!serviceAccountBase64) {
+    if (!VITE_FIREBASE_SERVICE_ACCOUNT) {
         throw new Error('Missing Firebase service account configuration');
     }
 
     try {
-        const decoded = Buffer.from(serviceAccountBase64, 'base64').toString();
+        const decoded = Buffer.from(VITE_FIREBASE_SERVICE_ACCOUNT, 'base64').toString();
         return JSON.parse(decoded);
     } catch (error) {
         console.error('Error parsing service account:', error);
