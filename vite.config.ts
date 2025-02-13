98e85f1753b525571a7a588f66c9107226820156
Igor Imperiali <igor.imperiali@gmail.com>
2025-02-13 15:44:13 -0300

    chore: fix build

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,40 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), tailwindcss()]
+	plugins: [sveltekit(), tailwindcss()],
+	build: {
+		target: 'esnext',
+		rollupOptions: {
+			external: [
+				'node:crypto',
+				'node:http',
+				'node:https',
+				'node:stream',
+				'node:assert',
+				'node:url',
+				'node:util',
+				'node:net',
+				'node:fs',
+				'node:path',
+				'node:events',
+				'node:buffer',
+				'node:zlib',
+				'node:http2',
+				'node:dns',
+				'node:tls',
+				'node:process'
+			]
+		}
+	},
+	ssr: {
+		noExternal: ['firebase', '@firebase/*']
+	},
+	optimizeDeps: {
+		exclude: ['firebase', '@firebase/*']
+	},
+	define: {
+		'process.env.NODE_ENV': '"production"',
+		'process.env': '{}',
+		'process.versions': '{}'
+	}
 });
